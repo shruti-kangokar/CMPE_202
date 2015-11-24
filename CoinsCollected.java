1e99feec1dc07c48089b0ce7cb7873ff371a8f2e
Piyush Patil <patilpiyushcsn@gmail.com>
2015-11-24 15:45:35 -0800

    piyush coin code

---
+++ CoinsCollected.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.Color;
+import java.awt.Font;
+
+/**
+ * Write a description of class CoinsCollected here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CoinsCollected extends Actor implements Observer
+{
+     private int score = 0;
+     protected FlappyBird flappy;
+     
+     public CoinsCollected(FlappyBird thesubject)
+    {
+        flappy = thesubject ;
+        setImage(new GreenfootImage(20,21));
+        show();
+    }
+    
+    public void update()
+    {
+        score++;
+        show();
+    }
+    
+    public void show()
+    {
+        GreenfootImage img = getImage();
+        img.clear();
+        img.setColor(Color.WHITE);
+        img.setFont(new Font("Trebuchet MS", Font.BOLD,13));
+        //img.setFont(img.getFont().deriveFont(Font.BOLD,15));
+        img.drawString(""+score,4,20);
+    }
+}
---
+++ Collectible.java
@@ -0,0 +1,12 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Collectible here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Collectible extends Actor
+{
+       
+}
